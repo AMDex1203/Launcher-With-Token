264bb9560c6b1aff435b8078bdd56bd73d1a8bb6
AMDex1203 <ajayida25@gmail.com>
2023-02-10 09:11:49 +0700

    Add project files.

---
+++ Modul.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointBlank.Launcher
+{
+    public class Modul
+    {
+        public static string LOGIN = "http://launcher.pb-memories.id/pblauncher/";
+        public static string WEB = "http://launcher.pb-memories.id/pblauncher/";
+        public static string UPDATEPATCH = "http://launcher.pb-memories.id/pblauncher/";
+        public static string Name = "PBLauncher";
+    }
+}
